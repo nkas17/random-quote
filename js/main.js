2f115998d75fc0cd4081a1af0e13b3d18c8a801a
nathan m weller <nkas17@gmail.com>
2017-12-26 20:49:55 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+// Shorthand for $( document ).ready()
+$(function() {
+	console.log( "loading quotes!" );
+	getQuote();
+
+ $('#next-quote').on('click', function(e) {
+	e.preventDefault();
+	getQuote();
+});
+
+
+ $('#tweet-quote').on('click', function() {
+	 window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+});
+
+});
+
+var currentQuote = "";
+var currentAuthor = "";
+
+function getQuote() {
+$.ajax( {
+		url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1',
+		headers: {
+			"X-Mashape-Key": "Zlim1IqWiUmsh012QAAHVeT9P1yMp1LXPNEjsnvLrkzvhRQSEO",
+			"Accept": "application/json"
+		},
+	
+		success: function(data) {
+
+			var dataJson = $.parseJSON(data);
+			// console.log(dataJson)
+			currentQuote = dataJson.quote;
+			currentAuthor = dataJson.author;
+			$('.quote-author').text('~ '+currentAuthor+' ~');
+			$('.quote-content').html('"'+currentQuote+'"');
+
+		},
+		cache: false
+	});
+}
